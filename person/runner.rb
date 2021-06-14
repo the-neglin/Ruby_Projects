27b86904f940078205ad9692fe7ffa4fcecc79b9
Nicholas Eglin <55520292+the-neglin@users.noreply.github.com>
2021-06-14 02:41:20 -0500

    Update runner.rb

--- person/runner.rb
+++ person/runner.rb
@@ -1,11 +1,11 @@
+#Test runner for person.rb
+
 require_relative 'person'
 
-#person = Person.new("Bob", "Thompson")
+person = Person.new("Bob", "Thompson")
 
 
-#person.last_name = "Jones"
-#person.create
-#person = Person.read person.person_id
-#Person.destroy person.person_id
-
-#Person.destroy 2130
+person.last_name = "Jones"
+person.create
+person = Person.read person.person_id
+Person.destroy person.person_id
