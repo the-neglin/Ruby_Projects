5d046dd50f288a4f7e0e835440577af7e96b5dba
Nicholas Eglin <55520292+the-neglin@users.noreply.github.com>
2021-06-14 02:39:49 -0500

    Add files via upload

---
+++ person/runner.rb
@@ -0,0 +1,11 @@
+require_relative 'person'
+
+#person = Person.new("Bob", "Thompson")
+
+
+#person.last_name = "Jones"
+#person.create
+#person = Person.read person.person_id
+#Person.destroy person.person_id
+
+#Person.destroy 2130
